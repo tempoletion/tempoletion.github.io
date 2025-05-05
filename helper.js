c46d10e16aa4aacdf4b48f025870a33d863588a4
tempoletion <tempoletionchanel@gmail.com>
2025-05-05 20:40:58 +0700

    update

--- helper.js
+++ helper.js
@@ -1,1 +1,58 @@
 //this is helper function
+
+//init dweeter func
+var playing = true;
+var c = document.getElementById('background');
+var x = c.getContext("2d");
+//c.width = 1920;
+//c.height = 1080;
+var S = Math.sin;
+var C = Math.cos;
+var T = Math.tan;
+
+var time = 0;
+var frame = 0;
+
+var R = function(r,g,b,a) {
+    a = a === undefined ? 1 : a;
+    return "rgba("+(r|0)+","+(g|0)+","+(b|0)+","+a+")";
+}
+
+var loop = function(){
+    if (playing){
+	requestAnimationFrame(loop);
+    }
+
+    time=frame/60;
+
+    if(time * 60 | 0 == frame - 1){
+	time += 0.000001;
+    }
+
+    frame++;
+
+    try{
+	u(time);
+	//displayerror("");
+    } catch (e) {
+        //displayError(e);
+        throw e;
+    }
+}
+
+function resizeCanvasToDisplaySize(canvas) {
+	  // Lookup the size the browser is displaying the canvas in CSS pixels.
+		  const displayWidth  = canvas.clientWidth;
+	  const displayHeight = canvas.clientHeight;
+	 
+	  // Check if the canvas is not the same size.
+		  const needResize = canvas.width  !== displayWidth ||
+		                     canvas.height !== displayHeight;
+	 
+	  if (needResize) {
+			    // Make the canvas the same size
+			    canvas.width  = displayWidth;
+			    canvas.height = displayHeight;
+			  } 
+	return needResize;
+}
--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-background = document.getElememtById("background");
+//main script of the page
+
+resizeCanvasToDisplaySize(c);
+
+var hw = c.width/2;
+var hh = c.height/2;
+
+//get most func fro my own dweeter
+
+//dweeter function
+function u(t){
+    c.width|=0
+    for(i=2e3;i--;)x.fillStyle=R(k=i/8,k,k),x.fillRect(hw+(j=i*1.023)*S(j)/2,hh+j*C(t+j|0)/2,s=i/19,s)
+}
+
+
+loop();
